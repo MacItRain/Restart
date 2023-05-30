1768e215c405ebc34bbdeda25c22259144fe3cc1
ChanYoung <a_b_ab@naver.com>
2023-05-30 19:26:34 +0900

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by 이찬영 on 2023/05/30.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
