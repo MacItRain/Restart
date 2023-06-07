8d35ba51880c758612875ed7eb7212c903a43670
ChanYoung <a_b_ab@naver.com>
2023-06-07 11:52:36 +0900

    ~7/6/2023 Before Animation

--- Restart/Views/HomeView.swift
+++ Restart/Views/HomeView.swift
@@ -8,8 +8,68 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @AppStorage("isOnboardingView") var isOnboardingView : Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        ZStack {
+            
+            
+            
+            VStack {
+
+                Spacer()
+                
+                ZStack {
+                    
+                    CircleImageView(color: Color.gray)
+                    
+                    Image("character-2")
+                        .resizable()
+                        .scaledToFit()
+                    
+                }
+                .padding()
+
+                Text("""
+                    The time that leads to mastery is
+                    dependant on the intensity of our
+                    focus
+                    """)
+                .multilineTextAlignment(.center)
+                .foregroundColor(Color.gray)
+                .font(.title3)
+                .fontWeight(.light)
+                .padding()
+                
+                Spacer()
+                
+                Button {
+                    isOnboardingView = true
+                } label: {
+                    
+                    ZStack {
+                        
+                        Capsule()
+                            .frame(width: 150, height: 60)
+                        
+                        HStack {
+                            
+                            Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                            
+                            Text("ReStart")
+                            
+                        }
+                        .foregroundColor(Color.white)
+                        .font(.title2)
+                        .bold()
+                    }
+                }
+                
+                
+            } // VStack
+        }
     }
 }
 
--- Restart/Views/OnboardingView.swift
+++ Restart/Views/OnboardingView.swift
@@ -8,13 +8,162 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    
+    @AppStorage("isOnboardingView") var isOnboardingView : Bool = true
+    
+    @State private var buttonWidth : Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset : CGFloat = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            
+            Color("ColorBlue").edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                
+                // Top Text View
+                
+                Text("Share.")
+                    .font(.system(size: 60))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .padding(.top)
+                
+                Text("""
+                    It's not how much we give but
+                    how much love we put into giving.
+                    """)
+                .multilineTextAlignment(.center)
+                .fontWeight(.light)
+                .foregroundColor(Color.white)
+                .font(.title3)
+                
+                // Middle
+                
+                ZStack {
+                    
+                    CircleImageView(color: Color.white)
+                       
+                    ZStack(alignment: .bottom) {
+                        
+                        Image("character-1")
+                            .resizable()
+                            .scaledToFit()
+                        
+                        Image(systemName: "arrow.left.and.right.circle")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                            .fontWeight(.ultraLight)
+                        
+                    }
+                    
+                } // ZStack
+                .padding()
+                
+                ZStack {
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.1))
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    
+                    HStack {
+                        
+                        ZStack {
+                            
+                            Capsule()
+                                .fill(Color.red.opacity(0.7))
+                                .frame(width: buttonOffset + 80)
+                            
+                            ZStack {
+                                
+                                Circle()
+                                    .fill(Color("ColorRed"))
+       
+                                Circle()
+                                    .fill(Color.black.opacity(0.15))
+                                    .padding(8)
+                                   
+                                Image(systemName: "chevron.right.2")
+                                    .font(.title2)
+                                    .foregroundColor(Color.white)
+                                    .bold()
+                                
+                            }
+                            .offset(x : buttonOffset)
+                            .gesture(
+                                DragGesture(coordinateSpace: .global)
+                                    .onChanged({ value in
+                                        
+                                        if value.translation.width > 0 && buttonOffset <= buttonWidth - 80 {
+                                            
+                                            buttonOffset = value.translation.width
+                                            
+                                        }
+                                        
+                                    })
+                                
+                                    .onEnded { _ in
+                                      withAnimation(Animation.easeOut(duration: 0.4)) {
+                                        if buttonOffset > buttonWidth / 2 {
+                                            
+                                          buttonOffset = buttonWidth - 80
+                                          isOnboardingView = false
+                                            
+                                        } else {
+                                       
+                                          buttonOffset = 0
+                                            
+                                        }
+                                      }
+                                    }
+                            )
+                        }
+                        
+                        Spacer()
+                        
+                    }
+                    
+                    Text("Get Started")
+                        .foregroundColor(Color.white)
+                        .bold()
+                    
+                }
+                .frame(width: buttonWidth, height: 80)
+                
+                Spacer()
+                
+            } // VStack
+            
+        } // ZStack : Background
     }
 }
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
         OnboardingView()
+    }
+}
+
+struct CircleImageView: View {
+    
+    let color : Color
+    
+    var body: some View {
+        
+        Circle()
+            .stroke(lineWidth: 40)
+            .frame(width: 280, height: 280)
+            .foregroundColor(color)
+            .opacity(0.2)
+        
+        Circle()
+            .stroke(lineWidth: 80)
+            .frame(width: 280, height: 280)
+            .foregroundColor(color)
+            .opacity(0.1)
+        
     }
 }
